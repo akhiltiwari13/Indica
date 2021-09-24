5ef4aba2166a19e5765b9d07c86c665718add858
akhil <akhiltiwari.13@gmail.com>
2021-09-24 15:47:42 +0530

    Project MacOS build

--- CSES.fiProblemSet/01_introductory_problems/chessboard_and_queens.cpp
+++ CSES.fiProblemSet/01_introductory_problems/chessboard_and_queens.cpp
@@ -2,17 +2,21 @@
 
 using namespace std;
 
-const int sq_size = 16;
-int count = 0;
-int column[sq_size];
-int left_diagonal[2 * (sq_size - 1) + 1];
-int right_diagonal[2 * (sq_size - 1) + 1];
+namespace global{
+    const int sq_size = 16;
+    int count = 0;
+    int column[sq_size];
+    int left_diagonal[2 * (sq_size - 1) + 1];
+    int right_diagonal[2 * (sq_size - 1) + 1];
+}
+
+using namespace global;
 
 void n_queen_solution(int r) {
   if (r == sq_size) {
     // one solution has been found as all the elements of the matrix/ chess
     // board have been visited.
-    count++;
+      global::count++;
   } else {
     for (int c = 0; c < sq_size; c++) {
       if (column[c] || left_diagonal[c + r] ||
@@ -34,6 +38,6 @@ void n_queen_solution(int r) {
 }
 int main(void) {
   n_queen_solution(0);
-  cout << "total solutions to the 4-queens problem are: " << count << endl;
+  cout << "total solutions to the 4-queens problem are: " << global::count << endl;
   return 0;
 }
