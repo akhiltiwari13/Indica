bef6862d77c6be208dabfec4ee25d7d7607b5d61
akhil <akhiltiwari.13@gmail.com>
2023-03-07 12:54:31 +0530

    explore cpp features & minor refactoring

--- CppFeatures/ExpFeatures.cpp
+++ CppFeatures/ExpFeatures.cpp
@@ -27,10 +27,9 @@ int main(int argc, char *argv[]) {
     // }
     auto v = getId();
     for(int i=0; i<10; ++i){
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    std::cout<<v<<std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::cout<<v<<std::endl;
     }
-    //some
-     return 0; }
+    return 0; }
 
      //what happens on returing reference to a local variable
---
+++ CppFeatures/stl.cpp
@@ -0,0 +1,54 @@
+#include <deque>
+#include <iostream>
+#include <array>
+#include <list>
+#include <forward_list>
+
+template <typename T>
+void printElements (const T& coll)
+{
+    for (const auto& elem : coll) {
+        std::cout << elem << " ";
+    }
+    std::cout<<std::endl;
+}
+
+int main(int argc, char* argv[]){
+
+    //deque
+    std::deque<float> dq1{4.0, 5.0, 6.0};
+    dq1.push_front(13.0);
+    dq1.push_front(16.0);
+    dq1.push_back(16.0);
+    printElements(dq1);
+    /* dq1.resize(1); */
+    /* printElements(dq1); */
+
+    //array
+    std::array<int,5> ar1{5,-23,1,-34, 56};
+    std::array<int,5> ar2{5,-23,1};
+    printElements(ar1);
+    printElements(ar2);
+    
+
+    //list
+    std::list<char> ls1;
+    for(auto c='a'; c<='z'; ++c){
+        ls1.push_back(c);
+    }
+
+    printElements(ls1);
+    std::cout<<"ls1.empty():"<< (ls1.empty()?"true":"false")<<std::endl;
+    std::cout<<"ls1.front():"<< ls1.front()<<std::endl;
+    std::cout<<"ls1.back():"<< ls1.back()<<std::endl;
+
+    //forward_list
+    std::forward_list<int> fls1{1,2,3};
+
+    //associative containers
+
+    //unordered containers
+
+
+    return 0;
+}
